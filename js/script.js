5e3789a9e7621066b8866add760b5ed61580fafb
Bernard-McGeever <Bernard.mcgeever@resonate.tech>
2021-03-11 16:52:38 +0000

    add js logic to print selected cards text to the console

--- js/script.js
+++ js/script.js
@@ -3,5 +3,12 @@ fetch('../data/CAH_Data.json')
     .then(data => retriveData(data));
 
 function retriveData(cardData) {
-    console.log(cardData);
+    const blackCard = cardData.data.black[Math.floor(Math.random() * cardData.data.black.length)];
+    const whiteCards = [];
+    for (i = 0; i < blackCard.pick; i++){
+        whiteCards.push(cardData.data.white[Math.floor(Math.random() * cardData.data.white.length)])
+    }
+
+    console.log(blackCard.text);
+    whiteCards.forEach(card => console.log(card.text));
 }
